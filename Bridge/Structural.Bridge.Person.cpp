3dc3f766eb490877c145324fea7ec8d511d9f4ac
arsha.shiri <a.shiri@plaxis.com>
2019-10-18 14:53:55 +0200

    Added bridge

---
+++ Bridge/Structural.Bridge.Person.cpp
@@ -0,0 +1,35 @@
+﻿// Pimple Idiom
+// hiding the implementation of a particular class by sticking it to its 
+// implementation.
+// By this we are hiding the details of the implementation.
+// The details go to the cpp files and are not provided to the client of the code.
+
+
+#include "Person.h"
+
+struct Person::PersonImpl
+{
+  void greet(Person* p);
+};
+
+void Person::PersonImpl::greet(Person* p)
+{
+  printf("hello %s", p->name.c_str());
+}
+
+Person::Person()
+  : impl(new PersonImpl)
+{
+}
+
+Person::~Person()
+{
+  delete impl;
+}
+
+void Person::greet()
+{
+  impl->greet(this);
+}
+
+
---
+++ Bridge/Structural.Bridge.Person.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include <string>
+
+struct Person
+{
+  std::string name;
+
+  struct PersonImpl;
+  PersonImpl *impl; // bridge - not necessarily inner class, can vary
+
+  Person();
+  ~Person();
+
+  // Implementation would be bridged in a separate class. 
+  void greet();
+};
